747afc66e246ee1eff0a702e52e22a789afea2d6
Administrator <Administrator@GRAZE>
2022-11-18 09:09:15 +0000

    Add project files.

---
+++ RainbowIce/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using Utilla;
+
+namespace RainbowIce
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        public GameObject loader;
+
+        void Start()
+        {
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("RainbowIce.Assets.loader");
+            AssetBundle rainbow = AssetBundle.LoadFromStream(str);
+            GameObject road = rainbow.LoadAsset<GameObject>("loader");
+            
+
+
+            GameObject.Find("Level/mountain/Geometry/mountainsideice").GetComponent<Renderer>().material = road.GetComponent<Renderer>().material;
+
+        }
+    }
+}
---
+++ RainbowIce/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace RainbowIce
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.rainbowice";
+        public const string Name = "RainbowIce";
+        public const string Version = "1.0.0";
+    }
+}
